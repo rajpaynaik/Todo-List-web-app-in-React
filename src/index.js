08b23abc022369e4c045750ed385b7df5a2986ee
rajpaynaik <rajpaynaik18@gmail.com>
2022-01-12 17:01:06 -0500

    adding redux-persist

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { configureStore } from './store';
-import App from './App.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import { configureStore } from "./store";
+import App from "./App.js";
+
+const store = configureStore();
+
+const persistor = persistStore(store);
 
 ReactDOM.render(
-    <Provider store={configureStore()} >
-        <App />
-    </Provider>,
-    document.getElementById('root'),
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
 );
